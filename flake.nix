9f575c334764ab932a82875a7ec24a3d5a273b8e
BrianNixLaptop <briannormant@gmail.com>
2024-11-14 23:05:52 -0500

    flake.nix trying jvm backend

--- flake.nix
+++ flake.nix
@@ -3,24 +3,27 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		# idris2-jvm.url = "github:mmhelloworld/idris-jvm?ref=v0.7.0";
 	};
 
 	outputs = { self, nixpkgs }: let
 		system = "x86_64-linux";
-	pkgs = import nixpkgs { inherit system; };
+		pkgs = import nixpkgs { inherit system; };
+		# idris2JVMPackages = idris2-jvm.packages.${system};
 	in {
 		devShells."${system}".default = pkgs.mkShell {
 			packages = with pkgs; [
-				idris2
-                idris2Packages.pack
-                gmp
+				# idris2JVMPackages.idris2
+                idris2
+				idris2Packages.pack
+				gmp
 				zsh
-                chez
+				chez
+				rlwrap
 			];
 			shellHook = ''
-                export SHELL=zsh
+				export SHELL=zsh
 				exec zsh
-				nvim
 				'';
 		};
 	};
